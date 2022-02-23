e12453d2f43df2266c74efdad76c912de95e2261
microlearnableSWF <96852808+microlearnableSWF@users.noreply.github.com>
2022-02-23 15:48:40 +0900

    hosting test

--- chapter0/run_flutter_project/kimshin12345/main.dart
+++ chapter0/run_flutter_project/kimshin12345/main.dart
@@ -1,16 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-void main() {
-  runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text(
-          'Hello, world!',
-          textDirection: TextDirection.ltr,
-        ),
-      ),
-    )
-  );
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Drag App',
+      home: Scaffold(body: MyHomePage()),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var list = List.generate(100, (index) => index.toString());
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return Dismissible(
+          key: Key(list[index]),
+          onDismissed: (event) {
+            setState(() {
+              list.removeAt(index);
+            });
+          },
+          child: ListTile(title: Center(child: Text(list[index].toString()))),
+          background: Container(
+            color: Colors.amber,
+          ),
+        );
+      },
+      itemCount: list.length,
+    );
+  }
 }
 
 void _result(bool success, [List<String> messages = const []]) {
