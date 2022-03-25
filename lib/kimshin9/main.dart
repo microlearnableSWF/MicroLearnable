3aa50cc52bfb422aeff6905f52b9eb4317e7e818
microlearnableSWF <96852808+microlearnableSWF@users.noreply.github.com>
2022-03-25 11:48:13 +0900

    hosting test

--- lib/kimshin9/main.dart
+++ lib/kimshin9/main.dart
@@ -5,7 +5,7 @@ dynamic main_test() {
 
 	return num;
 }
-
+// 테스트 중입니다.
 
 // 김신 원본 저장함
 void main() {
@@ -21,6 +21,20 @@ void main() {
     _result(false, ['Tried calling main_test(), but received an exception: ${e.runtimeType}']);
   }
 }
+
+void _result(bool success, [List<String> messages = const []]) {
+  // Join messages into a comma-separated list for inclusion in the JSON array.
+  final joinedMessages = messages.map((m) => '"$m"').join(',');
+  print('__TESTRESULT__ {"success": $success, "messages": [$joinedMessages]}');
+}
+
+// Ensure we have at least one use of `_result`.
+var resultFunction = _result;
+
+// Placeholder for unimplemented methods in dart-pad exercises.
+// ignore: non_constant_identifier_names, sdk_version_never
+Never TODO([String message = '']) => throw UnimplementedError(message);
+
 
 void _result(bool success, [List<String> messages = const []]) {
   // Join messages into a comma-separated list for inclusion in the JSON array.
